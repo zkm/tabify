06db47d5fcb0822d0720bfe917b5bfbe314f9601
ZKM <me@zachschneider.com>
2012-04-20 14:07:24 -0500

    first commit

---
+++ jquery.tabify.js
@@ -0,0 +1,35 @@
+(function($) {
+	$.fn.tabify = function(options) {
+		var defaults = {};
+		
+		var options = $.extend(defaults, options);
+		
+		return this.each(function(){
+			var self = $(this);
+			
+			$(this).find(".window").hide();
+			$(this).find(".window:first").show();
+			
+			$(this).find("ul li:first").addClass('active');
+			
+			$(this).find("ul li a").click(function(){
+												   
+				self.find("li").removeClass('active');
+				self.find(".window").hide();
+				
+				$(this).parent().addClass('active');		
+				var id = $(this).attr('href');
+				$(id).show();
+				
+				return false;
+				void(0);
+				
+			});
+		});
+		
+	}
+})(jQuery);
+
+$(document).ready(function(){
+	$(".tabs").tabify();	   
+});

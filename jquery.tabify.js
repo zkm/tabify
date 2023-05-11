ad2a5ed9d1952ec4ee748100f82d25934f8e2eab
Zach Schneider <me@zachschneider.com>
2023-05-11 09:46:54 -0500

    Refresh with latest jQuery version

--- jquery.tabify.js
+++ jquery.tabify.js
@@ -1,35 +1,34 @@
-(function($) {
-	$.fn.tabify = function(options) {
-		var defaults = {};
-		
-		var options = $.extend(defaults, options);
-		
-		return this.each(function(){
-			var self = $(this);
-			
-			$(this).find(".window").hide();
-			$(this).find(".window:first").show();
-			
-			$(this).find("ul li:first").addClass('active');
-			
-			$(this).find("ul li a").click(function(){
-												   
-				self.find("li").removeClass('active');
-				self.find(".window").hide();
-				
-				$(this).parent().addClass('active');		
-				var id = $(this).attr('href');
-				$(id).show();
-				
-				return false;
-				void(0);
-				
-			});
-		});
-		
-	}
+(function ($) {
+  $.fn.tabify = function (options) {
+    var defaults = {};
+
+    var options = $.extend(defaults, options);
+
+    return this.each(function () {
+      var self = $(this);
+
+      $(this).find(".window").hide();
+      $(this).find(".window:first").show();
+
+      $(this).find("ul li:first").addClass("active");
+
+      $(this)
+        .find("ul li a")
+        .click(function () {
+          self.find("li").removeClass("active");
+          self.find(".window").hide();
+
+          $(this).parent().addClass("active");
+          var id = $(this).attr("href");
+          $(id).show();
+
+          return false;
+          void 0;
+        });
+    });
+  };
 })(jQuery);
 
-$(document).ready(function(){
-	$(".tabs").tabify();	   
+$(document).ready(function () {
+  $(".tabs").tabify();
 });
